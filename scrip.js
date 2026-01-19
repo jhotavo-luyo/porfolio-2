a921df00474364e3126f448d9a8a61cd0753288f
jhotavo <jhotavoenperador.simismo@gmail.com>
2026-01-18 23:43:41 -0500

    desarrollo de 90% del layaut responsivo

--- scrip.js
+++ scrip.js
@@ -3,12 +3,11 @@
 // ****************************************************
 const btn__menu = document.querySelector("#btn__menu");
 const barraContactos = document.querySelector("#contacto");
-
 btn__menu.addEventListener("click",()=>{
     // agregamos estilos
-    if (barraContactos.style.display === "none") {
+    if (barraContactos.style.display == "none") {
         barraContactos.style.display = "flex";
-    } else {
+    }else {
         barraContactos.style.display = "none";
     }
     // preventDefault();
