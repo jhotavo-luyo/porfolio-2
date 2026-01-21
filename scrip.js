9a5c3f584d7ac1d2c3418b40ecbcd45d714cf2e2
jhotavo <jhotavoenperador.simismo@gmail.com>
2026-01-21 01:04:05 -0500

    entrega de la fase 4 de 4 terminado, de aqui para adelante solo sera limpieza de codigo o implementacion de funciones

--- scrip.js
+++ scrip.js
@@ -13,3 +13,25 @@ if (barraContactos.style.display === "none" ||btn__principal.style.display === "
 }
 btn__menu.addEventListener("click",handleContact);
 btn__principal.addEventListener("click",handleContact);
+
+// ****************************************************
+// Modal
+// ****************************************************
+const openModalBtn = document.getElementById('btn__modal__info');
+const closeModalBtn = document.getElementById('close-modal-btn');
+const modalOverlay = document.getElementById('modal-overlay');
+
+openModalBtn.addEventListener('click', () => {
+    modalOverlay.classList.add('active');
+});
+
+closeModalBtn.addEventListener('click', () => {
+    modalOverlay.classList.remove('active');
+});
+
+modalOverlay.addEventListener('click', (event) => {
+    if (event.target === modalOverlay) {
+        modalOverlay.classList.remove('active');
+    }
+});
+
